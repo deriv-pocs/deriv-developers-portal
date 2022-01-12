173a34693af775010bdd58a649e8c79d65a027ec
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-01-12 17:10:42 +0100

    updating checkboxes properly in update app form

--- registration_api.js
+++ registration_api.js
@@ -608,22 +608,32 @@ const go_update_mode = (...app) => {
     const update_read_scope = document.getElementById('read-scope');
     if (scopes.includes('read')) {
         update_read_scope.checked = true;
+    } else {
+        update_read_scope.checked = false;
     }
     const update_trade_scope = document.getElementById('trade-scope');
     if (scopes.includes('trade')) {
         update_trade_scope.checked = true;
+    } else {
+        update_trade_scope.checked = false;
     }
     const update_trading_information_scope = document.getElementById('trading_information-scope');
     if (scopes.includes('trading_information')) {
         update_trading_information_scope.checked = true;
+    } else {
+        update_trading_information_scope.checked = false;
     }
     const update_payments_scope = document.getElementById('payments-scope');
     if (scopes.includes('payments')) {
         update_payments_scope.checked = true;
+    } else {
+        update_payments_scope.checked = false;
     }
     const update_admin_scope = document.getElementById('admin-scope');
     if (scopes.includes('admin')) {
         update_admin_scope.checked = true;
+    } else {
+        update_admin_scope.checked = false;
     }
 }
 
