de52f8b8518355b63af5f097c5b2719472834081
Adam Rybinski <adam.rybinski@regentmarkets.com>
2022-04-19 16:48:56 +0200

    Move skeleton to seperate component

--- AppManagement.jsx
+++ AppManagement.jsx
@@ -1,7 +1,9 @@
 import { useMemo } from 'react';
 import { useTable, useSortBy } from 'react-table';
 import './AppRegistration.scss';
+import styles from './AppManagement.module.scss';
 import { useApps } from './useApps';
+import SkeletonText from './components/SkeletonText/SkeletonText';
 
 const columns = [
     {
@@ -37,53 +39,51 @@ export default function AppManagement() {
         data: table_data,
     }, useSortBy);
     return (
-        <div className="manage-apps">
-            <div className="manage-apps-table">
-                <table {...getTableProps()}>
-                    <thead>
-                        {headerGroups.map(headerGroup => (
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {headerGroup.headers.map(column => (
-                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                        {column.render("Header")}
-                                        <span>
-                                            {column.isSorted
-                                                ? column.isSortedDesc
-                                                    ? '↓'
-                                                    : '↑'
-                                                : ''}
-                                        </span>
-                                    </th>
-                                ))}
+        <div className={styles.manageApps}>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render("Header")}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? '↓'
+                                                : '↑'
+                                            : ''}
+                                    </span>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {isLoading && <SkeletonRows />}
+                    {rows.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                                })}
                             </tr>
-                        ))}
-                    </thead>
-                    <tbody {...getTableBodyProps()}>
-                        {isLoading && <SkeletonRows />}
-                        {rows.map((row, i) => {
-                            prepareRow(row);
-                            return (
-                                <tr {...row.getRowProps()}>
-                                    {row.cells.map(cell => {
-                                        return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                                    })}
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
 
 const SkeletonRows = () => {
     const Skeleton = () => (<tr>
-    <td><div className="skeleton"></div></td>
-    <td><div className="skeleton"></div></td>
-    <td><div className="skeleton"></div></td>
-    <td><div className="skeleton"></div></td>
-    <td><div className="skeleton"></div></td>
+        <td><SkeletonText /></td>
+        <td><SkeletonText /></td>
+        <td><SkeletonText /></td>
+        <td><SkeletonText /></td>
+        <td><SkeletonText /></td>
     </tr>);
     // return 5 skeletons
     return [...Array(5)].map((_, i) => <Skeleton key={i} />);
