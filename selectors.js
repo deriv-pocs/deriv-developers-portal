8fa485ba1de987a7453314eb3851082a0884e997
Hubert Koster <hubertkoster@Huberts-MacBook-Pro.local>
2022-05-10 12:25:16 +0200

    sorting

--- selectors.js
+++ selectors.js
@@ -5,6 +5,6 @@ export const isDeletingAppModal = (state) => state.matches('registration.logged_
 export const isMobileSelector = (state) => state.matches('responsive.mobileS')
 || state.matches('responsive.mobileM') || state.matches('responsive.mobileL') || state.matches('responsive.mobileTablet');
 export const isRegisterSuccessSelector = (state) => state.matches('registration.logged_in.register_tab.success_modal') || state.matches('registration.logged_in.update_mode.success_modal');
-export const isRegisterTabIdle = (state) => state.matches('registration.logged_in.register_tab.idle');
 export const isRegisterErrorSelector = (state) => state.matches('registration.logged_in.register_tab.error_modal') || state.matches('registration.logged_in.update_mode.error_modal');
+export const isRegisterTabIdle = (state) => state.matches('registration.logged_in.register_tab.idle');
 export const isUpdateModeSelector = (state) => state.matches('registration.logged_in.update_mode');
